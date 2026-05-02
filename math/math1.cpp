707c76fac155db7ae44ae61394e5d08942988b8c
Likhith <rlikhith2004@gmail.com>
2026-05-02 08:54:06 +0530

    Additional

---
+++ math/math1.cpp
@@ -0,0 +1,15 @@
+/*
+What is a divisor?
+A number is 'a' factor(divisor) of 'b' if 'a' divides 'b' exactly without a remainder
+
+
+BRUTE FORCE METHOD:
+check every number from 2 to n 
+vector<int> divisors;
+for (int i = 1;i<=n;i++){
+    if (n%i==0) divisors.push_back(i);
+}
+-> Time complexity: O(n)
+-> Works for n<=1e6
+-> Too slow for Cp when n<=1e12
+*/
