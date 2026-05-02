475464cc9a99ef8767ce10203674d898293852cc
Likhith <rlikhith2004@gmail.com>
2026-05-02 08:57:43 +0530

    Additional

--- math/math1.cpp
+++ math/math1.cpp
@@ -6,10 +6,11 @@ A number is 'a' factor(divisor) of 'b' if 'a' divides 'b' exactly without a rema
 BRUTE FORCE METHOD:
 check every number from 2 to n 
 vector<int> divisors;
-for (int i = 1;i<=n;i++){
+for (int i = 1; i<=n; i++){
     if (n%i==0) divisors.push_back(i);
 }
 -> Time complexity: O(n)
+-> Space complexity: O(n)
 -> Works for n<=1e6
 -> Too slow for Cp when n<=1e12
 */
