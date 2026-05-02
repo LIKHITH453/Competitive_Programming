29064357b740d325210d33f252a7332aa7a05724
Likhith <rlikhith2004@gmail.com>
2026-05-02 09:17:33 +0530

    Additional

--- math/math1.cpp
+++ math/math1.cpp
@@ -13,4 +13,17 @@ for (int i = 1; i<=n; i++){
 -> Space complexity: O(n)
 -> Works for n<=1e6
 -> Too slow for Cp when n<=1e12
+
+Optimized method:
+Creation of pairs n, n/d
+vector<long long> divisors;
+for (int i = 1; i*i<=n; i++){
+    if (n%i==0) {
+        divisors.push_back(i);
+        if (i!=n/i) divisors.push_back(n/i);
+    }
+}
+-> Time complexity: O(sqrt(n))
+-> Space complexity: O(sqrt(n))
+-> Works for n<=1e12
 */
